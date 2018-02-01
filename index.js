557225f6c7eef59053dc26e946f05ece48d99558
Rock <279375210@qq.com>
2018-02-02 00:12:08 +0800

    i dont't want to use third library but it's diffcult

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
 const http = require('http');
 const fs = require('fs');
+const formidable = require('formidable');
+const util = require('util');
 const localhost = '127.0.0.1';
 const port = '3000';
 
@@ -12,8 +16,9 @@ const html = `
   </head>
   <body>
     <form method="post" action="/file" enctype='multipart/form-data'>
+      <input type="text" name="name"><br>
       <label for="upload">上传文件</label>
-      <input id="upload" type="file" />
+      <input id="upload" type="file" name="file"/>
       <input type="submit" value="提交" />
     </form>
   </body>
@@ -33,14 +38,21 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  let buf = '';
-  req.on('data', (data) => {
-    buf += data;
-  })
+  const form = new formidable.IncomingForm();
+  form.encoding = "utf-8";
+  form.uploadDir = __dirname + '/tmp';
+  form.keepExtensions = true; 
+  form.parse(req, function(err, fields, files) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write('received upload:\n\n');
+    res.end(util.inspect({fields: fields, files: files}));
 
-  req.on('end', () => {
-    console.log(buf.toString());
-  })
+    fs.rename(files.file.path, `${__dirname}/tmp/${files.file.name}`, function (err) {
+      if (err) {
+          console.info(err);
+      }
+    });
+  });
 })
 
 server.listen(port, localhost, () => {
